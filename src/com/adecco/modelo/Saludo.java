8ed22ecefd1a53501152f9186b24c4cf0aa2db9d
GabrielaGP <cursos@gabrielagomezpuente.es>
2023-03-23 14:07:52 +0100

    Ejercicio 1

---
+++ src/com/adecco/modelo/Saludo.java
@@ -0,0 +1,7 @@
+package com.adecco.modelo;
+
+public class Saludo {
+    public String saludar(){
+        return "Bienvenidos al curso de Java";
+    }
+}
---
+++ src/com/adecco/presentacion/Main.java
@@ -0,0 +1,12 @@
+package com.adecco.presentacion;
+
+
+import com.adecco.modelo.Saludo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Saludo objSaludo = new Saludo();
+        System.out.println(objSaludo.saludar());
+    }
+}
